93200106bcff85240eed130f38a49c9b3b2f4b08
Om  Patil <omsandeeppatil02@gmail.com>
2024-09-14 21:24:14 +0530

    Update page.tsx

--- src/app/(main)/calender/page.tsx
+++ src/app/(main)/calender/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { format, startOfMonth, endOfMonth, eachDayOfInterval, isSameMonth, isSameDay, addMonths, subMonths } from 'date-fns';
 

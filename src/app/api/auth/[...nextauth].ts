d509dc08013410078f91488df03ef0dceecae6b0
Om  Patil <omsandeeppatil02@gmail.com>
2024-09-20 22:55:05 +0530

    Update [...nextauth].ts

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,23 +1,26 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google"; // example for Google provider
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
     // Add other providers here
   ],
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    async session({ session, user }) {
+    async session({ session, token, user }) {
       if (session?.user) {
         session.user.id = user.id; // Add user ID to session
       }
       return session;
     },
   },
-});
+  // Optionally, you can add more settings such as session strategy, pages, etc.
+};
+
+export default NextAuth(authOptions);
